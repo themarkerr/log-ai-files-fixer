d99b89a2a2970be782887d7d82daf244a1c06c10
Leonardo Silveira <sombriks@gmail.com>
2023-12-13 13:22:23 -0300

    works on old data conversion

--- just-download.sh
+++ just-download.sh
@@ -9,3 +9,23 @@ then
   echo "usage: $0 bucket-name-to-fix"
   exit 1
 fi
+
+echo "checking if bucket exists"
+
+aws s3 ls $BUCKET --profile markerr
+
+if [ $? -ne 0 ]
+then 
+  echo "bucket $BUCKET does not exists"
+  exit 1
+fi
+
+echo "dump $BUCKET..."
+
+rm -rf $BUCKET
+mkdir $BUCKET
+cd $BUCKET
+aws s3 cp s3://$BUCKET . --recursive --profile markerr
+cd $CWD
+
+echo "done!"
